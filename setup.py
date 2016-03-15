f025728bb4e9e36192f287517882bc09dcfef4cc
Pierre Leveau <pierre@zen.ly>
2016-03-15 12:57:03 +0100

    setup: dummy setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "test_package",
+    description = "test package",
+    license="",
+    url = "",
+    long_description = "",
+    version = "1.0.0",
+    author = "Pierre Leveau",
+    author_email = "pierre@pierre.pierre",
+)
